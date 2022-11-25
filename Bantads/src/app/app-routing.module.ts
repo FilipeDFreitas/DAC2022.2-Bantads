f4ebc1d0364fccad5b79e43ee8599f68c80472c9
CLAUDIA BASTCHEN <clau.eng.mec@gmail.com>
2022-11-24 21:40:22 -0300

    ajusteClientev02

--- Bantads/src/app/app-routing.module.ts
+++ Bantads/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { IndexComponent } from './cliente/index/index.component';
+import { ClienteDadosComponent } from './cliente/cliente-dados/cliente-dados.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', 
+      redirectTo: 'clientes/index', 
+      pathMatch: 'full' },
+    { path: 'clientes', 
+      redirectTo: 'clientes/index' },
+    { path: 'clientes/index', 
+      component:  IndexComponent },
+    { path: 'clientes/cliente-editar', 
+      component: ClienteDadosComponent }
+      
+];
+
     @NgModule({
         imports: [RouterModule.forRoot(routes)],
         exports: [RouterModule]
--- Bantads/src/app/cliente/cliente.module.ts
+++ Bantads/src/app/cliente/cliente.module.ts
@@ -2,10 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ClienteService } from './services/cliente.service';
+import { ClienteDadosComponent } from './cliente-dados/cliente-dados.component';
+import { DepositarComponent } from './depositar/depositar.component';
+import { ExtratoComponent } from './extrato/extrato.component';
+import { IndexComponent } from './index/index.component';
+import { SaldoComponent } from './saldo/saldo.component';
+import { SaqueComponent } from './saque/saque.component';
+import { TransferenciaComponent } from './transferencia/transferencia.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ClienteDadosComponent,
+    DepositarComponent,
+    ExtratoComponent,
+    IndexComponent,
+    SaldoComponent,
+    SaqueComponent,
+    TransferenciaComponent
+  ],
   imports: [
     CommonModule
   ],
